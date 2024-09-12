e8a07a199ab8fe634c229d55df1dd80972b0a00c
HafizhMA <muhammadazhr29@gmail.com>
2024-09-12 17:06:57 +0700

    install and using react-type-animation

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <div className="navbar tes">
+        <div className="navbar navbar-style">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -21,7 +21,7 @@ const Header = () => {
                     </div>
                     <ul
                         tabIndex={0}
-                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow text-black">
                         <li><a>Item 1</a></li>
                         <li><a>Item 3</a></li>
                     </ul>
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { TypeAnimation } from 'react-type-animation';
 
 const Home = () => {
     return (
-        <div className='h-screen'>Home</div>
+        <div className='h-screen container px-6 bg-slate-50'>
+            <div className='grid grid-cols-2'>
+                <div className='flex flex-col justify-center text-4xl'>
+                    <TypeAnimation
+                        className='font-4xl whitespace-pre-line'
+                        sequence={[
+                            `Hi, My Name Is\nHafizh Muhammad Azhar`,
+                            1000,
+                            '',
+                        ]}
+                        repeat={Infinity}
+                    />
+                </div>
+                <div className='flex flex-col justify-center'>
+                    <p className='text-justify'>I am an Informatics Engineering student with a passion for web development. I am dedicated to consistently improving my programming skills and solving complex problems through practical, hands-on projects. My focus is on both frontend and backend development. I am proficient in using React.js and Tailwind CSS for frontend development, and Express.js for backend development. For databases, I have experience working with MySQL and PostgreSQL.</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
