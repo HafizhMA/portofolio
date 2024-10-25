036b1b200727e14d8e8bfa647eb461d93e3f3c8a
HafizhMA <muhammadazhr29@gmail.com>
2024-10-25 17:45:11 +0700

    remove visit button and change card as anchor href

--- src/components/home-components/ProjectsSection.jsx
+++ src/components/home-components/ProjectsSection.jsx
@@ -1,87 +1,74 @@
 import React from 'react'
+import { Link } from 'react-scroll';
 
 const ProjectsSection = () => {
+    const projects = [
+        {
+            name: 'UsedUp',
+            description: 'A website for buying and selling used items that already has a wide range of buying and selling features.',
+            imgSrc: '/usedup.png',
+            stack: ['React js', 'Tailwind CSS', 'Express js', 'Prisma', 'PostgreSQL'],
+            link: 'https://usedup.up.railway.app/'
+        },
+        {
+            name: 'Electronic',
+            description: 'A website for selling electronic goods that is integrated with the Raja Ongkir API and the Midtrans payment gateway.',
+            imgSrc: '/electronic.png',
+            stack: ['React js', 'Tailwind CSS', 'Express js', 'Prisma', 'PostgreSQL'],
+            link: 'https://electronic-orpin-alpha.vercel.app/'
+        },
+        {
+            name: 'Weather',
+            description: 'A website providing the latest weather forecast information integrated with the OpenWeatherMap API.',
+            imgSrc: '/weather.png',
+            stack: ['HTML', 'CSS', 'JavaScript'],
+            link: 'https://weather-simple.up.railway.app/'
+        },
+        {
+            name: 'Task List',
+            description: 'A simple website for recording your activities. You can update, delete, input, and cross off tasks that have been completed.',
+            imgSrc: '/task.png',
+            stack: ['HTML', 'CSS', 'JavaScript'],
+            link: 'https://task-list-one-phi.vercel.app/'
+        },
+        {
+            name: 'Calculator BMI',
+            description: 'A calculator designed to determine body proportions.',
+            imgSrc: '/calculatorbmi.png',
+            stack: ['HTML', 'CSS', 'JavaScript'],
+            link: 'https://calculator-bmi-brown.vercel.app/'
+        },
+        {
+            name: 'Calculator',
+            description: 'A calculator, in general, is a device or tool used for performing mathematical calculations.',
+            imgSrc: '/calculator.png',
+            stack: ['HTML', 'CSS', 'JavaScript'],
+            link: 'https://calculator-eight-delta-17.vercel.app/'
+        }
+    ];
+
     return (
         <div className='min-h-screen'>
             <p className='font-bold text-2xl mb-3'>Projects</p>
             <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5'>
-                <div className='border-4 border-slate-500 rounded p-1 space-y-2 flex flex-col'>
-                    <div className='h-[50%]'>
-                        <img className='object-cover' src="/usedup.png" alt="Image Description" />
-                    </div>
-                    <div className='px-1 h-[30%]'>
-                        <p className='font-bold text-xl'>UsedUp</p>
-                        <p>A website for buying and selling used items that already has a wide range of buying and selling features.</p>
-                    </div>
-                    <div className='self-end p-2 h-[20%]'>
-                        <a href='https://usedup.up.railway.app/' className='border-2 border-slate-400 px-2 py-1 rounded font-semibold text-slate-500'>Visit</a>
-                    </div>
-                </div>
-
-                <div className='border-4 border-slate-500 rounded p-1 space-y-2 flex flex-col'>
-                    <div className='h-[50%]'>
-                        <img className='object-cover' src="/electronic.png" alt="Image Description" />
-                    </div>
-                    <div className='px-1 h-[30%]'>
-                        <p className='font-bold text-xl'>Electronic</p>
-                        <p>A website for selling electronic goods that is integrated with the Raja Ongkir API and the Midtrans payment gateway.</p>
-                    </div>
-                    <div className='self-end p-2 h-[20%]'>
-                        <a href='https://electronic-orpin-alpha.vercel.app/' className='border-2 border-slate-400 px-2 py-1 rounded font-semibold text-slate-500'>Visit</a>
-                    </div>
-                </div>
-
-                <div className='border-4 border-slate-500 rounded p-1 space-y-2 flex flex-col'>
-                    <div className='h-[50%]'>
-                        <img className='object-cover' src="/weather.png" alt="Image Description" />
-                    </div>
-                    <div className='px-1 h-[30%]'>
-                        <p className='font-bold text-xl'>Weather</p>
-                        <p>A website providing the latest weather forecast information integrated with the OpenWeatherMap API.</p>
-                    </div>
-                    <div className='self-end p-2 h-[20%]'>
-                        <a href='https://weather-simple.up.railway.app/' className='border-2 border-slate-400 px-2 py-1 rounded font-semibold text-slate-500'>Visit</a>
-                    </div>
-                </div>
-
-                <div className='border-4 border-slate-500 rounded p-1 space-y-2 flex flex-col'>
-                    <div className='h-[50%]'>
-                        <img className='object-cover' src="/task.png" alt="Image Description" />
-                    </div>
-                    <div className='px-1 h-[30%]'>
-                        <p className='font-bold text-xl'>Task List</p>
-                        <p>A simple website for recording your activities. You can update, delete, input, and cross off tasks that have been completed.</p>
-                    </div>
-                    <div className='self-end p-2 h-[20%]'>
-                        <a href='https://task-list-one-phi.vercel.app/' className='border-2 border-slate-400 px-2 py-1 rounded font-semibold text-slate-500'>Visit</a>
-                    </div>
-                </div>
-
-                <div className='border-4 border-slate-500 rounded p-1 space-y-2 flex flex-col'>
-                    <div className='h-[50%]'>
-                        <img className='object-cover' src="/calculatorbmi.png" alt="Image Description" />
-                    </div>
-                    <div className='px-1 h-[30%]'>
-                        <p className='font-bold text-xl'>Calculator BMI</p>
-                        <p>A calculator designed to determine body proportions</p>
-                    </div>
-                    <div className='self-end p-2 h-[20%]'>
-                        <a href='https://calculator-bmi-brown.vercel.app/' className='border-2 border-slate-400 px-2 py-1 rounded font-semibold text-slate-500'>Visit</a>
-                    </div>
-                </div>
-
-                <div className='border-4 border-slate-500 rounded p-1 space-y-2 flex flex-col'>
-                    <div className='h-[50%]'>
-                        <img className='object-cover' src="/calculator.png" alt="Image Description" />
-                    </div>
-                    <div className='px-1 h-[30%]'>
-                        <p className='font-bold text-xl'>Calculator</p>
-                        <p>A calculator, in general, is a device or tool used for performing mathematical calculations</p>
-                    </div>
-                    <div className='self-end p-2 h-[20%]'>
-                        <a href='https://calculator-eight-delta-17.vercel.app/' className='border-2 border-slate-400 px-2 py-1 rounded font-semibold text-slate-500'>Visit</a>
-                    </div>
-                </div>
+                {projects.map((project, index) => (
+                    <a href={project.link} key={index} className='cursor-pointer border-4 border-slate-500 rounded p-1 flex flex-col pb-8'>
+                        <div className=''>
+                            <img className='object-cover' src={project.imgSrc} alt={`${project.name} Image`} />
+                        </div>
+                        <div className='h-[50%] mt-3'>
+                            <div className='px-1 h-[68%]'>
+                                <p className='font-bold text-xl'>{project.name}</p>
+                                <p>{project.description}</p>
+                            </div>
+                            <div className='flex flex-wrap space-x-3 p-1 text-sm mt-2'>
+                                {project.stack.map((tech, i) => (
+                                    <p key={i} className='stack'>{tech}</p>
+                                ))}
+                            </div>
+                        </div>
+                    </a>
+                ))}
             </div>
         </div>
     )
