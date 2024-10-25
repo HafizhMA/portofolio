e17029465c4102928d1904bf32e7d542f721d5e6
HafizhMA <muhammadazhr29@gmail.com>
2024-10-25 17:59:29 +0700

    refactor

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -3,7 +3,7 @@ import HeroSection from '../home-components/HeroSection';
 
 const Home = () => {
     return (
-        <div className='mx-6 bg-slate-50'>
+        <div className='px-6 bg-slate-50'>
             <HeroSection />
         </div>
     )
