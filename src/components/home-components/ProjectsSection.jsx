c7dc98f8d5b4b95c66e36cbdf7cf153d46b76129
HafizhMA <muhammadazhr29@gmail.com>
2024-10-25 16:32:52 +0700

    add projects

--- src/components/home-components/ProjectsSection.jsx
+++ src/components/home-components/ProjectsSection.jsx
@@ -7,6 +7,19 @@ const ProjectsSection = () => {
             <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5'>
                 <div className='border-4 border-slate-500 rounded p-1 space-y-2 flex flex-col'>
                     <div className='h-[50%]'>
+                        <img className='object-cover' src="/usedup.png" alt="Image Description" />
+                    </div>
+                    <div className='px-1 h-[30%]'>
+                        <p className='font-bold text-xl'>UsedUp</p>
+                        <p>A website for buying and selling used items that already has a wide range of buying and selling features.</p>
+                    </div>
+                    <div className='self-end p-2 h-[20%]'>
+                        <a href='https://usedup.up.railway.app/' className='border-2 border-slate-400 px-2 py-1 rounded font-semibold text-slate-500'>Visit</a>
+                    </div>
+                </div>
+
+                <div className='border-4 border-slate-500 rounded p-1 space-y-2 flex flex-col'>
+                    <div className='h-[50%]'>
                         <img className='object-cover' src="/electronic.png" alt="Image Description" />
                     </div>
                     <div className='px-1 h-[30%]'>
@@ -41,6 +54,32 @@ const ProjectsSection = () => {
                     </div>
                     <div className='self-end p-2 h-[20%]'>
                         <a href='https://task-list-one-phi.vercel.app/' className='border-2 border-slate-400 px-2 py-1 rounded font-semibold text-slate-500'>Visit</a>
+                    </div>
+                </div>
+
+                <div className='border-4 border-slate-500 rounded p-1 space-y-2 flex flex-col'>
+                    <div className='h-[50%]'>
+                        <img className='object-cover' src="/calculatorbmi.png" alt="Image Description" />
+                    </div>
+                    <div className='px-1 h-[30%]'>
+                        <p className='font-bold text-xl'>Calculator BMI</p>
+                        <p>A calculator designed to determine body proportions</p>
+                    </div>
+                    <div className='self-end p-2 h-[20%]'>
+                        <a href='https://calculator-bmi-brown.vercel.app/' className='border-2 border-slate-400 px-2 py-1 rounded font-semibold text-slate-500'>Visit</a>
+                    </div>
+                </div>
+
+                <div className='border-4 border-slate-500 rounded p-1 space-y-2 flex flex-col'>
+                    <div className='h-[50%]'>
+                        <img className='object-cover' src="/usedup.png" alt="Image Description" />
+                    </div>
+                    <div className='px-1 h-[30%]'>
+                        <p className='font-bold text-xl'>Calculator</p>
+                        <p>A calculator, in general, is a device or tool used for performing mathematical calculations</p>
+                    </div>
+                    <div className='self-end p-2 h-[20%]'>
+                        <a href='https://calculator-eight-delta-17.vercel.app/' className='border-2 border-slate-400 px-2 py-1 rounded font-semibold text-slate-500'>Visit</a>
                     </div>
                 </div>
             </div>
